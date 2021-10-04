70a4f4d37def4692d084a7830cc06341079592b4
SANEDROID123 <62328357+prajwal080501@users.noreply.github.com>
2021-10-04 13:43:51 +0530

    added navbar in hero section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,20 +1,42 @@
-import React from 'react';
+import React from "react";
 import Container from "react-bootstrap/Container";
-import Navbar from 'react-bootstrap/Navbar'
+import Navbar from "react-bootstrap/Navbar";
+import Nav from 'react-bootstrap/Nav'
+import { NavDropdown } from "react-bootstrap";
 
-
-const Hero = ( {handleLogout}) => { 
-    return (
-        <Container className="hero-page">
-            
-            <nav>
-                <h2>Welcome</h2>
-                <button onClick={handleLogout}>Logout</button>
-                </nav>
-            
-            
-
-        </Container>
-    )
-}
+const Hero = ({ handleLogout }) => {
+  return (
+    <Container className="hero-page">
+      <nav>
+        <h2>Welcome</h2>
+        <Navbar bg="light" expand="lg">
+          <Container>
+            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#link">Link</Nav.Link>
+                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">
+                  Signin React
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">
+                    Something
+                  </NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.4">
+                    Separated link
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+        <button onClick={handleLogout}>Logout</button>
+      </nav>
+    </Container>
+  );
+};
 export default Hero;
